2f0b4ac07491622835e1b0be19fbf2296d504d7e
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 16:29:19 -0300

    componente app

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+//class é uma palavra reservada, usar por exemplo className
+export default props => (
+    <div className="container">
+        <h1>Teste</h1>
+    </div>
+)
