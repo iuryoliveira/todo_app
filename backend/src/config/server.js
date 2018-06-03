dacb76a94f9fa3e5cfbe263ea6d9fed7340f8995
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 15:44:38 -0300

    criando o arquivos cors e habilitando no servidor

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,12 @@ const port = 3004
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 //Middlewares
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`)
