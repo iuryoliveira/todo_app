528268ed060a464c760f06818098c5a6b9fbf256
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 16:37:33 -0300

    Criação dos componentes todo e about

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,10 +2,14 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 //class é uma palavra reservada, usar por exemplo className
 export default props => (
     <div className="container">
-        <h1>Teste</h1>
+        <h1>Componente app</h1>
+        <Todo />
+        <About />
     </div>
 )
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Componente Todo </h1>
+            </div>
+        )
+    }
+}
