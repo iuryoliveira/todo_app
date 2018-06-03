e73daa04cd7ca9c488170ba66f797aaccf0dcdb7
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 19:58:14 -0300

    Criação do template menu e chamada no app.jsx

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,10 +4,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 //class é uma palavra reservada, usar por exemplo className
 export default props => (
     <div className="container">
+        <Menu />
         <h1>Componente app</h1>
         <Todo />
         <About />
