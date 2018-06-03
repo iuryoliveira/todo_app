d60cd46b47f8e8d838ba42a2d38226e5666efede
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 20:46:06 -0300

    Criação do pageHeader, alteração do conteúdo de todo e about para usarem pageHeader

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -2,12 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Tarefas</h1>
+                <PageHeader name="Tarefas" small="Cadastro"/>
             </div>
         )
     }
