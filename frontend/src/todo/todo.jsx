71328a88e39b98a7118bed35ae6b6233155be153
Iury Magalhaes <iurylei@hotmail.com>
2018-06-03 23:31:15 -0300

    criação do formulário e lista de todo

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -3,12 +3,16 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"/>
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <form>
+            <h1> Formulário </h1>
+        </form>
+    </div>
+)
